76873792bf2c1aea189395a1628688a82f69922b
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-15 17:45:38 +0000

    finish 2023.12.16(5)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get 'books/:id' => 'books#show', as: 'book'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
   get '/top' => 'homes#top'
   get 'books' => 'books#index'

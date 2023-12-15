65b3acaec6c173863d6909d5059b8953717f9b20
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-15 08:40:30 +0000

    finish 2023.12.15(3)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # get 'books/new'
   # post 'books' => 'books#create'
-  # get 'books/index'
+  # get 'books' => 'books#index'
   # get 'books/show'
   # get 'books/edit'
 
   get '/top' => 'homes#top'
-  resources :books
+  get 'books' => 'books#index'
+  resources :books, only: [:show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

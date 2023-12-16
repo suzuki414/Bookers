f5c3a33b8a267dc22ca8313ca861ce500c80502b
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-16 03:07:38 +0000

    start 2023.12.16(6)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get '/top' => 'homes#top'
   get 'books' => 'books#index'
+  
   # resources :books, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

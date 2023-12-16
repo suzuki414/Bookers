892529b56ba108c624c904ba907d6b5d0e79fd68
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-16 13:46:29 +0000

    start 2023.12.16(7)

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,10 @@ Rails.application.routes.draw do
   # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   # get 'books/:id' => 'books#show', as: 'book'
   # patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
   get '/top' => 'homes#top'
-  # get 'books' => 'books#index'
-  
-  # resources :books, only: [:show, :new, :create, :edit, :update, :destroy]
+  root to: 'home#top'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

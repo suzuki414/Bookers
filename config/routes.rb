cd87ddb5e38712c0a943a058b9cf95bbb0410474
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-16 08:14:41 +0000

    finish 2023.12.16(6)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'books/:id' => 'books#show', as: 'book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+  # get 'books/new'
+  # post 'books' => 'books#create'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
   get '/top' => 'homes#top'
-  get 'books' => 'books#index'
+  # get 'books' => 'books#index'
   
   # resources :books, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :books
